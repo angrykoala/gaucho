2e5e384121f44309fa2dce74f341a7f560425301
angrykoala <demiurgosoft@hotmail.com>
2016-12-08 00:04:34 +0000

    Added config file, electron build ready, close #8

--- main.js
+++ main.js
@@ -2,16 +2,30 @@
 
 const {app, BrowserWindow} = require('electron');
 
+const config=require('./config.json');
+
+function isDevEnv(){
+    return process.env.NODE_ENV==="dev";
+}
+
 //Global reference to window
 let win;
 
 function createWindow () {
-  win = new BrowserWindow({width: 950, height: 600});
+    let winSize={
+        width: config.windowSize[0],
+        height: config.windowSize[1]
+    };
+    if(isDevEnv()){
+        winSize.width+=config.devToolsSize;
+    }
+    
+  win = new BrowserWindow(winSize);
 
   win.loadURL(`file://${__dirname}/index.html`);
 
  //Comment to remove devTools
- win.webContents.openDevTools();
+ if(isDevEnv()) win.webContents.openDevTools();
 
   win.on('closed', () => {
     win = null;
