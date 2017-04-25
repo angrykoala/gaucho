33038db3f85dbc4519f060caa254fc39a1b2dd55
angrykoala <demiurgosoft@hotmail.com>
2017-04-25 02:19:24 +0200

    suite tests added, close #63

--- test/suite.test.js
+++ test/suite.test.js
@@ -5,19 +5,36 @@ const sinon = require('sinon');
 
 const Suite = require('../app/renderer/suite');
 const Task = require('../app/renderer/task');
+const TaskStatus = require('../app/common/task_status');
 
 describe("Suite", () => {
     let taskStub;
     let testSuite;
 
+    function createStub(taskStub) {
+        sinon.stub(taskStub, "getData").returns("taskStub");
+        sinon.stub(taskStub, "run").callsFake(() => {
+            taskStub.status = TaskStatus.running;
+        });
+        sinon.stub(taskStub, "stop").callsFake(() => {
+            taskStub.status = TaskStatus.idle;
+        });
+    }
+
+    function restoreStub(taskStub) {
+        taskStub.getData.restore();
+        taskStub.run.restore();
+        taskStub.stop.restore();
+    }
+
     beforeEach(() => {
         taskStub = new Task("test", "", "command");
         testSuite = new Suite("Test");
-        sinon.stub(taskStub, "getData").returns("taskStub");
+        createStub(taskStub);
     });
 
     afterEach(() => {
-        taskStub.getData.restore();
+        restoreStub(taskStub);
     });
 
     it("Create New Suite", () => {
@@ -36,12 +53,40 @@ describe("Suite", () => {
         assert.strictEqual(testSuite.length, 0);
     });
 
-    it.skip("Run Tasks", () => {
+    it("Run Tasks", () => {
+        const taskStub2 = new Task("test", "", "command");
+        createStub(taskStub2);
+        taskStub2.status = TaskStatus.running;
 
+        testSuite.addTask(taskStub);
+        testSuite.addTask(taskStub2);
+        assert.isFalse(taskStub.isRunning());
+        assert.isTrue(taskStub2.isRunning());
+        testSuite.runAll();
+        assert.isTrue(taskStub.isRunning());
+        assert.isTrue(taskStub2.isRunning());
+        assert.isTrue(taskStub.run.called);
+        assert.isFalse(taskStub2.run.called);
+
+        restoreStub(taskStub2);
     });
 
-    it.skip("Stop All Tasks", () => {
+    it("Stop All Tasks", () => {
+        const taskStub2 = new Task("test", "", "command");
+        createStub(taskStub2);
+        taskStub2.status = TaskStatus.running;
 
+        testSuite.addTask(taskStub);
+        testSuite.addTask(taskStub2);
+        assert.isFalse(taskStub.isRunning());
+        assert.isTrue(taskStub2.isRunning());
+        testSuite.stopAll();
+        assert.isFalse(taskStub.isRunning());
+        assert.isFalse(taskStub2.isRunning());
+        assert.isFalse(taskStub.stop.called);
+        assert.isTrue(taskStub2.stop.called);
+
+        restoreStub(taskStub2);
     });
 
     it("Replace Tasks", () => {
