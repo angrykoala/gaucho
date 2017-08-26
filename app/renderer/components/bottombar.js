f4f9be0ae4010d2d439f730be14cbdac606505d6
grg121 <fnm121grg@gmail.com>
2017-08-26 15:27:20 +0200

    add a simple bottom-bar to start solving #125

---
+++ app/renderer/components/bottombar.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const AppStatus = require('../app_status');
+
+
+
+module.exports = {
+    
+    props: ['suite'],    
+    data() {
+        return {
+            AppStatus: AppStatus
+        };
+    },
+    template: `
+    <div class="bottombar">
+        <nav class="navbar navbar-inverse">
+             <div id="left">
+                Total Tasks: 10
+            </div>
+            <div id="right">
+                Running Tasks: 0
+            </div>
+    </nav> 
+    </div>
+    `,
+   
+};
