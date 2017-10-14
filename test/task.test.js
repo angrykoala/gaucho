d0c0549875051916373d63bd9d9d0d28bb6b28d6
andres <andres@shuttlecloud.com>
2017-10-14 19:14:11 +0200

    minor refactoring in tasks

--- test/task.test.js
+++ test/task.test.js
@@ -79,13 +79,13 @@ describe("Tasks", () => {
 
     it("Update Execution Time", () => {
         assert.throws(() => {
-            testTask.updateElapsedTime();
+            testTask._updateElapsedTime();
         });
         assert.isNull(testTask.elapsedTime);
 
         testTask.run(() => {}, () => {});
         assert.doesNotThrow(() => {
-            testTask.updateElapsedTime();
+            testTask._updateElapsedTime();
         });
         assert.isNumber(testTask.elapsedTime);
     });
