84e34b880779c74bb9df03d3ba209e60cf779926
Gagan M <gagan.blr@gmail.com>
2017-10-25 22:11:16 +0530

    show timer based on config

--- app/renderer/task.js
+++ app/renderer/task.js
@@ -1,85 +1,103 @@
 "use strict";
 
-const EventEmitter = require('events');
+const EventEmitter = require("events");
 
-const yerbamate = require('yerbamate');
+const yerbamate = require("yerbamate");
 
-const TaskStatus = require('../common/task_status');
-const TaskTimer = require('../common/timer');
-
+const TaskStatus = require("../common/task_status");
+const TaskTimer = require("../common/timer");
+const AppStatus = require("./app_status");
+const showTimer = AppStatus.config.showTimer;
 
 const TaskEvents = new EventEmitter();
-TaskTimer(TaskEvents, 1000);
 
 class Task {
-    constructor(title, path, command) {
-        this.title = title.trim() || "";
-        this.command = command || "";
-        this.path = path || "";
-        this.status = TaskStatus.idle;
+  constructor(title, path, command) {
+    this.title = title.trim() || "";
+    this.command = command || "";
+    this.path = path || "";
+    this.status = TaskStatus.idle;
 
-        this.beginTime = null;
-        this.finishTime = null;
-        this.elapsedTime = null;
-        this.onTimeUpdate = null;
+    this.beginTime = null;
+    this.finishTime = null;
+    this.elapsedTime = null;
+    this.onTimeUpdate = null;
+  }
+
+  run(stdout, done) {
+    const showTimer = AppStatus.config.showTimer;
+    showTimer && TaskTimer(TaskEvents, 1000);
+
+    if (this.isRunning()) {
+      throw new Error("Trying to run task without stopping it first");
     }
+    this.status = TaskStatus.running;
+    this.proc = yerbamate.run(
+      this.processCommand(),
+      this.path,
+      {
+        stderr: stdout,
+        stdout: stdout
+      },
+      code => {
+        if (this.status !== TaskStatus.stopped)
+          this.status = yerbamate.successCode(code)
+            ? TaskStatus.ok
+            : TaskStatus.error;
 
-    run(stdout, done) {
-        if (this.isRunning()) {
-            throw new Error("Trying to run task without stopping it first");
+        if (showTimer) {
+          this.finishTime = Date.now();
+          this.updateElapsedTime();
+          TaskEvents.removeListener("time-update", this.onTimeUpdate);
         }
-        this.status = TaskStatus.running;
-        this.beginTime = Date.now();
-        this.finishTime = null;
-        this.proc = yerbamate.run(this.processCommand(), this.path, {
-                stderr: stdout,
-                stdout: stdout
-            },
-            (code) => {
-                if (this.status !== TaskStatus.stopped) this.status = yerbamate.successCode(code) ? TaskStatus.ok : TaskStatus.error;
-                this.finishTime = Date.now();
-                this.updateElapsedTime();
-                TaskEvents.removeListener("time-update", this.onTimeUpdate);
-                done();
-            });
-        this.onTimeUpdate = () => {
-            this.updateElapsedTime();
-        };
-        TaskEvents.on("time-update", this.onTimeUpdate);
+        done();
+      }
+    );
+
+    if (showTimer) {
+      this.beginTime = Date.now();
+      this.finishTime = null;
+
+      this.onTimeUpdate = () => {
         this.updateElapsedTime();
-    }
+      };
 
-    stop(cb) {
-        if (this.isRunning()) {
-            yerbamate.stop(this.proc, cb);
-            this.status = TaskStatus.stopped;
-        } else if (cb) cb();
+      TaskEvents.on("time-update", this.onTimeUpdate);
+      this.updateElapsedTime();
     }
+  }
 
-    isRunning() {
-        return this.status === TaskStatus.running;
-    }
+  stop(cb) {
+    if (this.isRunning()) {
+      yerbamate.stop(this.proc, cb);
+      this.status = TaskStatus.stopped;
+    } else if (cb) cb();
+  }
 
-    getData() {
-        let res = {
-            title: this.title,
-            command: this.command,
-        };
-        if (this.path !== "") res.path = this.path;
-        return res;
-    }
+  isRunning() {
+    return this.status === TaskStatus.running;
+  }
 
-    updateElapsedTime() {
-        if (this.beginTime === null) throw new Error("Error, cant update time");
-        let finishTime = this.finishTime;
-        if (finishTime === null) finishTime = Date.now();
+  getData() {
+    let res = {
+      title: this.title,
+      command: this.command
+    };
+    if (this.path !== "") res.path = this.path;
+    return res;
+  }
 
-        this.elapsedTime = Math.trunc((finishTime - this.beginTime) / 1000);
-    }
+  updateElapsedTime() {
+    if (this.beginTime === null) throw new Error("Error, cant update time");
+    let finishTime = this.finishTime;
+    if (finishTime === null) finishTime = Date.now();
 
-    processCommand() {
-        return this.command.replace(/(^|\s)sudo($|\s)/g, "$1pkexec$2");
-    }
+    this.elapsedTime = Math.trunc((finishTime - this.beginTime) / 1000);
+  }
+
+  processCommand() {
+    return this.command.replace(/(^|\s)sudo($|\s)/g, "$1pkexec$2");
+  }
 }
 
 Task.TaskStatus = TaskStatus;
