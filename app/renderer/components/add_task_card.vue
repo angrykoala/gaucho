442aa69cc51c798257c4a7f441020a0dc56eb194
Andrés <angrykoala@outlook.es>
2018-06-01 01:16:52 +0200

    minor add task card improvements

--- app/renderer/components/add_task_card.vue
+++ app/renderer/components/add_task_card.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <div class="columns is-mobile" @click="cardSelected">
-            <div class="column">
-                <p class="has-text-centered">Add New Task</p>
+        <div class="columns is-mobile task-card" @click="cardSelected">
+            <div class="column has-text-centered">
+                <a>Add New Task</a>
             </div>
         </div>
         <div v-if="edit" class="columns is-mobile">
@@ -33,3 +33,20 @@ module.exports = {
 };
 
 </script>
+
+<style lang="scss" scoped>
+.column{
+    margin-bottom: 0;
+    margin-top: 0;
+}
+.task-card{
+    margin-bottom: 0;
+    margin-top: 0;
+    padding-bottom: 0;
+    padding-left: 10px;
+    padding-right:10px;
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    border-color: #e2e2e2;
+}
+</style>
