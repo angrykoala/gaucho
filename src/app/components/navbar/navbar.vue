0f62e2ad087f4f8fcafaa3fd26bd5be2031c559b
Dhfernando <39521923+DhFernando@users.noreply.github.com>
2019-10-02 16:59:53 +0530

    make some change

--- src/app/components/navbar/navbar.vue
+++ src/app/components/navbar/navbar.vue
@@ -5,7 +5,7 @@
                 <div class="navbar-item unselectable">
                     <img class="logo-icon" src="../../../../resources/logos/gaucho_logo.ico">
                     <h1 class="title is-4">Gaucho</h1>
-                    <h2 class="beta-subtitle is-4">Beta</h2>
+                    <h2 class="beta-subtitle is-4">BETA</h2>
                 </div>
             </div>
             <div class="navbar-menu is-active">
