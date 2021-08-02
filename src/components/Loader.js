63a4282223d76fc4790b806548b912d4e7b50fe8
angrykoala <angrykoala@outlook.es>
2021-08-02 13:46:09 +0200

    Download buttons

---
+++ src/components/Loader.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Loader() {
+  return (<div class="loader"></div>)
+}
--- src/pages/download.js
+++ src/pages/download.js
@@ -4,17 +4,68 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Loader from '../components/Loader';
 import GithubMiniApi from '../components/GithubMiniApi'
 import styles from './download.module.css';
 
-import Tabs from '@theme/Tabs';import TabItem from '@theme/TabItem';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 
 function AssetLink({name,browser_download_url}) {
   return (
     <li>
-    <a href={browser_download_url} download>{name}</a>
-  </li>
-)
+    <Link
+      className={clsx("button primary-button button--lg",styles.downloadButton)}
+      to={browser_download_url}>
+      {name}
+    </Link>
+    </li>
+  )
+}
+
+function DownloadTabs({linuxAssets, windowsAssets, macAssets}){
+  return (
+    <Tabs defaultValue="linux" className={clsx(styles.downloadTabs)}  values={[
+      {label: 'Linux', value: 'linux'},
+      {label: 'Windows', value: 'windows'},
+      {label: 'Mac', value: 'mac'},
+    ]}>
+    <TabItem value="linux">
+    <ul>
+      {linuxAssets.map((asset, idx) => (
+        <AssetLink key={idx} {...asset} />
+      ))}
+    </ul>
+    </TabItem>
+    <TabItem value="windows">
+    <ul>
+      {windowsAssets.map((asset, idx) => (
+        <AssetLink key={idx} {...asset} />
+      ))}
+    </ul>
+    </TabItem>
+    <TabItem value="mac">
+    <ul>
+      {macAssets.map((asset, idx) => (
+        <AssetLink key={idx} {...asset} />
+      ))}
+    </ul>
+    </TabItem>
+    </Tabs>
+  )
+}
+
+function DownloadHeader({version}) {
+  const {siteConfig} = useDocusaurusContext();
+  const versionText=version?`Version ${version}`:""
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className={clsx("hero__title", styles.heroBannerTitle)}>Download</h1>
+        <i className={clsx("hero__subtitle", styles.heroBannerSubtitle)}>{versionText}</i>
+      </div>
+    </header>
+  );
 }
 
 
@@ -29,61 +80,36 @@ export default function Download() {
     setReleaseLoaded(true)
   }, [])
   const assets=releaseData.assets || []
-  const version=releaseData.tag_name
+  const version=releaseData.tag_name || undefined
 
-  const linuxAssets=assets.filter(ass=>{
-    return /linux|\.deb|\.snap|\.AppImage|\.rpm/i.test(ass.name)
-  })
-  const windowsAssets=assets.filter(ass=>{
-    return /windows|dos|\.exe/i.test(ass.name)
-  })
-  const macAssets=assets.filter(ass=>{
-    return /mac/i.test(ass.name)
-  })
+
+  const assetProps={
+    linuxAssets: assets.filter(ass=>{
+      return /linux|\.deb|\.snap|\.AppImage|\.rpm/i.test(ass.name)
+    }),
+    windowsAssets: assets.filter(ass=>{
+      return /windows|dos|\.exe/i.test(ass.name)
+    }),
+    macAssets: assets.filter(ass=>{
+      return /mac/i.test(ass.name)
+    })
+  }
 
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title} Download`}
       description={siteConfig.tagline}>
+      <DownloadHeader version={version} />
       <main class={styles.downloadMain}>
-        <h1>Download</h1>
-          { !releaseLoaded && <div class="loader"></div>}
+          { !releaseLoaded && <Loader/>}
           { releaseLoaded && <>
-            <p><i>Version {version}</i></p>
-
-          <Tabs  defaultValue="linux"  values={[
-            {label: 'Linux', value: 'linux'},
-            {label: 'Windows', value: 'windows'},
-            {label: 'Mac', value: 'mac'},
-          ]}>
-          <TabItem value="linux">
-          <ul>
-            {linuxAssets.map((asset, idx) => (
-              <AssetLink key={idx} {...asset} />
-            ))}
-          </ul>
-          </TabItem>
-          <TabItem value="windows">
-          <ul>
-            {windowsAssets.map((asset, idx) => (
-              <AssetLink key={idx} {...asset} />
-            ))}
-          </ul>
-          </TabItem>
-          <TabItem value="mac">
-          <ul>
-            {macAssets.map((asset, idx) => (
-              <AssetLink key={idx} {...asset} />
-            ))}
-          </ul>
-          </TabItem>
-          </Tabs>
+            <div className="container">
+              <DownloadTabs {...assetProps}/>
+            </div>
         </>}
-          <a href="https://github.com/angrykoala/gaucho/releases">Check all releases</a>
+          <a href="https://github.com/angrykoala/gaucho/releases">Check other releases</a>
       </main>
     </Layout>
   );
 }
-
-// You can download Gaucho for Linux, Windows, and Mac at https://github.com/angrykoala/gaucho/releases
