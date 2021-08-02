4c1a94f0f1585288bc80c2e750c3142c7f1a6086
angrykoala <angrykoala@outlook.es>
2021-08-02 03:46:52 +0200

    download gaucho wip

--- src/components/GithubMiniApi.js
+++ src/components/GithubMiniApi.js
@@ -1,5 +1,5 @@
 export default {
-  getLastRelease(){
+  getLastRelease(){ //TODO use latest when not pre-release
     return fetch("https://api.github.com/repos/angrykoala/gaucho/releases?per_page=1", {
       headers:{
         Accept: "application/vnd.github.v3+json",
--- src/pages/download.js
+++ src/pages/download.js
@@ -7,10 +7,12 @@ import HomepageFeatures from '../components/HomepageFeatures';
 import GithubMiniApi from '../components/GithubMiniApi'
 import styles from './download.module.css';
 
+import Tabs from '@theme/Tabs';import TabItem from '@theme/TabItem';
+
 function AssetLink({name,browser_download_url}) {
   return (
     <li>
-    <a href={browser_download_url}>{name}</a>
+    <a href={browser_download_url} download>{name}</a>
   </li>
 )
 }
@@ -18,32 +20,67 @@ function AssetLink({name,browser_download_url}) {
 
 export default function Download() {
   const [releaseData, setReleaseData] = useState({});
+  const [releaseLoaded, setReleaseLoaded] = useState(false);
   useEffect(async () => {
     const res=await GithubMiniApi.getLastRelease()
     const data=await res.json()
     console.log(data[0])
     setReleaseData(data[0])
+    setReleaseLoaded(true)
   }, [])
   const assets=releaseData.assets || []
   const version=releaseData.tag_name
+
+  const linuxAssets=assets.filter(ass=>{
+    return /linux|\.deb|\.snap|\.AppImage|\.rpm/i.test(ass.name)
+  })
+  const windowsAssets=assets.filter(ass=>{
+    return /windows|dos|\.exe/i.test(ass.name)
+  })
+  const macAssets=assets.filter(ass=>{
+    return /mac/i.test(ass.name)
+  })
 
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title} Download`}
       description={siteConfig.tagline}>
-      <main className={clsx(styles.downloadPage)}>
-        <div>
-          <h1>Download Gaucho {version}</h1>
+      <main class={styles.downloadMain}>
+        <h1>Download</h1>
+          { !releaseLoaded && <div class="loader"></div>}
+          { releaseLoaded && <>
+            <p><i>Version {version}</i></p>
 
+          <Tabs  defaultValue="linux"  values={[
+            {label: 'Linux', value: 'linux'},
+            {label: 'Windows', value: 'windows'},
+            {label: 'Mac', value: 'mac'},
+          ]}>
+          <TabItem value="linux">
           <ul>
-            {assets.map((asset, idx) => (
+            {linuxAssets.map((asset, idx) => (
               <AssetLink key={idx} {...asset} />
             ))}
           </ul>
-
+          </TabItem>
+          <TabItem value="windows">
+          <ul>
+            {windowsAssets.map((asset, idx) => (
+              <AssetLink key={idx} {...asset} />
+            ))}
+          </ul>
+          </TabItem>
+          <TabItem value="mac">
+          <ul>
+            {macAssets.map((asset, idx) => (
+              <AssetLink key={idx} {...asset} />
+            ))}
+          </ul>
+          </TabItem>
+          </Tabs>
+        </>}
           <a href="https://github.com/angrykoala/gaucho/releases">Check all releases</a>
-        </div>
       </main>
     </Layout>
   );
