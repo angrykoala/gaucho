3603711a3173636b228c7e7d0d54242601071a47
angrykoala <angrykoala@outlook.es>
2022-04-14 15:37:25 +0100

    sass styles

--- src/renderer/app.vue
+++ src/renderer/app.vue
@@ -1,11 +1,20 @@
 <template>
-<p>Hello World2</p>
-<button @click="onClick">Click me!</button>
+<main>
+    <p> Hello world</p>
+    <!-- <div class="main-view" v-if="!settingsMenu">
+        <navbar></navbar>
+        <suite v-for="(suite, index) of suites" v-show="selectedSuite===index" v-if="editMode || selectedSuite===index" :key="index" :suite="index"></suite>
+        <bottom-bar v-if="showBottomBar"></bottom-bar>
+    </div>
+    <settings-menu v-else /> -->
+</main>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {
+    defineComponent
+} from 'vue';
 
 export default defineComponent({
     methods: {
