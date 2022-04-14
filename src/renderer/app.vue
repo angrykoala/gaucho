6ea38f356394d1d8c1fe61f82dfaeb2a81aacc97
angrykoala <angrykoala@outlook.es>
2022-04-14 15:09:04 +0100

    vue-tsc support

--- src/renderer/app.vue
+++ src/renderer/app.vue
@@ -1,4 +1,17 @@
 <template>
 <p>Hello World2</p>
-
+<button @click="onClick">Click me!</button>
 </template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    methods: {
+        onClick() {
+            console.log("ON CLICK")
+        }
+    }
+});
+</script>
